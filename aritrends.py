fa126320b41b02866b46c55394016d5924635e6a
Abhineet Raj <82033840+abhineetraj1@users.noreply.github.com>
2023-06-30 16:37:58 +0530

    Added pdf merger feature

--- aritrends.py
+++ aritrends.py
@@ -7,7 +7,7 @@ import webbrowser
 import py7zr
 import rarfile
 import os
-
+import PyPDF2
 
 def rnm():
     return datetime.now().strftime("%Y%m%d%H%M%S")
@@ -56,5 +56,30 @@ class aritrends:
     @staticmethod
     def show_credits():
         print("This project is developed by Abhineet Raj (GitHub: @abhineetraj1).")
+    @staticmethod
+    def merge_pdfs(filenames, output_filename):
+        merged_pdf = PyPDF2.PdfFileMerger()
+        try:
+            # Merge all PDF files
+            for filename in filenames:
+                with open(filename, 'rb') as file:
+                    merged_pdf.append(file)
+        except FileNotFoundError:
+            print(f"Error: File '{filename}' not found.")
+            return
+        except PyPDF2.PdfReadError:
+            print(f"Error: Invalid PDF file '{filename}'.")
+            return
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return
+        try:
+            # Save the merged PDF to the output file
+            with open(output_filename, 'wb') as output_file:
+                merged_pdf.write(output_file)
+        except Exception as e:
+            print(f"An error occurred while saving the merged PDF: {e}")
+            return
+        print(f"Merged PDFs saved to '{output_filename}'.")
 
 print("Welcome to file processing library.\nType 'aritrends.show_credits()' or 'aritrends.open_github()' for more information")
